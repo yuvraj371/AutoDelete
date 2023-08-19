4e8593f3ae9221d6decfbec30c4338852aa06610
yuvraj371 <139117085+yuvraj371@users.noreply.github.com>
2023-08-19 15:13:35 +0530

    Update server.py

--- utils/server.py
+++ utils/server.py
@@ -22,7 +22,7 @@ app = Flask(__name__)
 
 @app.route("/")
 async def route():
-    return "<h1>Check <a href='https://github.com/arun017s/AutoDelete'>AutoDelete</a></h1>"
+    return "<h1>Check <a href='https://github.com/yuvraj371/AutoDelete'>AutoDelete</a></h1>"
 
 if __name__ == "__main__":     
    app.run()
