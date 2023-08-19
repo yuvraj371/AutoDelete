73ffc574c448a8ecd0673948ca68f8e8b8ebf357
yuvraj371 <139117085+yuvraj371@users.noreply.github.com>
2023-08-19 15:47:37 +0530

    Update main.py

--- main.py
+++ main.py
@@ -39,7 +39,7 @@ async def delete(user, message):
     except Exception as e:
        print(str(e))
 
-@User.on_message(filters.regex("!start") & filters.private)
+@User.on_message(filters.regex("/start") & filters.private)
 async def start(user, message):
     await message.reply("Hi, I'm alive!")
 
