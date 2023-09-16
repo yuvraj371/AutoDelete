5e512d4f0e19c605905633d60c18812a24a2c0d3
yuvraj371 <139117085+yuvraj371@users.noreply.github.com>
2023-09-16 17:00:07 +0530

    Update info.py

--- utils/info.py
+++ utils/info.py
@@ -18,13 +18,13 @@
 
 import os
 
-API_ID       = int(os.environ.get("API_ID", "21938068"))
-API_HASH     = os.environ.get("API_HASH", "c18fd98f3e58484df0aecd95a3d5a6a9")
-BOT_TOKEN    = os.environ.get("BOT_TOKEN", "6471432077:AAHhFOnRuhUTuBy3gnNbLE11DZ_aEl00llU")
-SESSION      = os.environ.get("SESSION", "BQFOv5QAWPlIUyFD5u5-haj_rUat7TGNU-lp5iv8wF1CCq7gPaibzW7PgYxUqkUBAlnyGtBTz6gOuPqtwGmHeJzWHH_rzco4ZdPDKCcUtXs5MTKC3yfvwJZ5cWlbg_WmMHYlaTXuq4-CgWM7gVE1kq0_P730J1wKZJCgyH-K3HEOzQkB1yVn6uJhGPydO8FxL8yNNBaU157jtV0qWvpe47-95mZk9sHdSGxnpjqzpAVEjFw7yCs1xthbNiKMkrcutbCiNps4jvb9sRpwNjd1vxao6PMExxHTcQYmZn50G_AhBFVqZD2IzGIFO6PMizxc5kjmEMrtKgwBugH_-dO6xVX7tt0nAAAAAAFY9mSPAA")
-TIME         = int(os.environ.get("TIME", 3600))
-CHATS        = [int(cht) for cht in os.environ.get("CHATS", "-1001920976113").split()]
+API_ID       = int(os.environ.get("API_ID", "10246965"))
+API_HASH     = os.environ.get("API_HASH", "ef650551668be9e53efb691180ce0880")
+BOT_TOKEN    = os.environ.get("BOT_TOKEN", "6690067645:AAHRqm4-dvU3s4gjS07d9CEFPdInWB6D2ak")
+SESSION      = os.environ.get("SESSION", "BQCcWzUAichAsdFVVzrkUZFZdivo5Mqp7J8xGj5ZoTUVz8OivMoVTD6AVXe0xGFOZpWkrE7cPLwg_H_KdBd6qX0iR2r7uA_Rw7uFBvk1ZriPA4UzIQnwyPmptM_s2nnkFxVouqei7zX1hRT-QHLqAdMBSy6SqecZj6uCyR8TC97q2c_YqW51nOPSuDBiGBIEFUW2jUo53ITmB6hf7SdsqY4_uLDLbOomP0XOligQXVSj8TYYx1a68IKjdjf50GZO4a3VQA5HlKCG7SVWmAqzlyQuJRgxpv0xilIziamQAw6ocd4GubfToI84bSrSPJfxSnrgLpitad2HxwoE6fNJoiNp3x5XOwAAAABQjsH4AA")
+TIME         = int(os.environ.get("TIME", 1200))
+CHATS        = [int(cht) for cht in os.environ.get("CHATS", "-1001699421068 -1001969653899 -1001940605857").split()]
 WHITE_LIST   = [int(wht) for wht in os.environ.get("WHITE_LIST", "").split()]
 BLACK_LIST   = [int(blk) for blk in os.environ.get("BLACK_LIST", "").split()]
-DATABASE_URI = os.environ.get("DATABASE_URI", "mongodb+srv://Autodeletebot:yuvraj178@autodeletebot.khceugr.mongodb.net/?retryWrites=true&w=majority")
+DATABASE_URI = os.environ.get("DATABASE_URI", "mongodb+srv://Autodeletebot:Dharamveer1100@cluster0.luyy59m.mongodb.net/?retryWrites=true&w=majority")
 PORT         = os.environ.get("PORT", "8080")
